619a25171ab3098e85d0e5524bf491da0b40fa3e
Ahmed Mansour <ahmed@mansour.fi>
2019-01-26 13:45:30 +0200

    refactor anchors to g-links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,21 +3,19 @@
     <h1 class="text-xs-center">Hello, I'm Ahmed Mansour! 👋</h1>
     <p>first paragraph about me.</p>
     <p>second paragraph about me.</p>
-    <p>
-      Would you like to hear more?
-      <a href="/about">Read my story</a>
+    <p>Would you like to hear more?
+      <g-link class="nav-link" to="/about">Read my stor</g-link>
     </p>
 
     <h3>Hear from me</h3>
 
-    <p>
-      I am maintaining a weekly short
-      <a href="/newletter">newsletter</a>.
+    <p>I am maintaining a weekly short
+      <g-link class="nav-link" to="/newletter">newsletter</g-link>
     </p>
 
     <p>Paragraph about the news letter</p>
     <p>Ready to join me?
-      <a href="/newsletter">Join the newsletter over here</a>.
+      <g-link class="nav-link" to="/newletter">Join the newsletter over here</g-link>
     </p>
   </Layout>
 </template>
