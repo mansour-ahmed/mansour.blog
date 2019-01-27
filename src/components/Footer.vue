a400b138e480eee560311c5db900e28e7d55148e
Ahmed Mansour <ahmed@mansour.fi>
2019-01-27 19:59:57 +0200

    add correct links to social icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,14 +3,14 @@
     <div class="content-center">
       <div class="headline font-weight-bold text-xs-center">Ahmed Mansour</div>
       <v-layout justify-center align-start pt-3 row>
-        <a class="anchor-without-border" href="https://www.linkedin.com/in/ahmedmansour03/" target="_blank">
-          <g-image class="pr-4" alt="Linkedin Profile" :src="linkedinUrl" width="50"/>
-        </a>
         <a
           class="anchor-without-border"
-          href="https://github.com/mansour-ahmed/"
+          href="https://www.linkedin.com/in/ahmedmansour03/"
           target="_blank"
         >
+          <g-image class="pr-4" alt="Linkedin Profile" :src="linkedinUrl" width="50"/>
+        </a>
+        <a class="anchor-without-border" href="https://github.com/mansour-ahmed/" target="_blank">
           <g-image class="pr-4" alt="Github Profile" :src="githubUrl" width="50"/>
         </a>
         <a class="anchor-without-border" href="https://twitter.com/AhmedMansour03/" target="_blank">
@@ -35,7 +35,7 @@ export default {
 
 <style scoped>
 .footer-container {
-  background-color: #EAEDED;
+  background-color: #eaeded;
 }
 a {
   border: none;
